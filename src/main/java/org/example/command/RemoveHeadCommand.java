e250a2c6debe28771f7e88ed14351a6b11eeccb4
konstantinzemskov <1500konstantin@gmail.com>
2023-04-11 16:00:39 +0300

    Add RemoveHeadCommand

---
+++ src/main/java/org/example/command/RemoveHeadCommand.java
@@ -0,0 +1,27 @@
+package org.example.command;
+
+import org.example.collection.MusicBandCollection;
+
+import java.util.List;
+
+public class RemoveHeadCommand extends CollectionCommand{
+    public RemoveHeadCommand(MusicBandCollection collection, CommandManager manager) {
+        super(collection, manager);
+    }
+
+    @Override
+    public void execute(List<String> args) {
+        if(collection.getListOfElements().size() == 0) return;
+        manager.getChannel().sendString(collection.getListOfElements().remove(0).toString());
+    }
+
+    @Override
+    public String getDescription() {
+        return "Выводит первый элемент в коллекции и удаляет его";
+    }
+
+    @Override
+    public String getName() {
+        return "remove_head";
+    }
+}
