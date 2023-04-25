ba363c6d75c0aaf87f2c5addc393ef75ef75a7f9
Seviginu <57951462+Seviginu@users.noreply.github.com>
2023-04-25 17:53:35 +0300

    make objects print more readable

--- src/main/java/org/example/collection/Printable.java
+++ src/main/java/org/example/collection/Printable.java
@@ -3,13 +3,21 @@ package org.example.collection;
 import java.lang.reflect.Field;
 
 public abstract class Printable {
+  private static final String ANSI_GREEN = "\u001B[32m";
+  private static final String ANSI_RESET = "\u001B[0m";
+
   @Override
   public String toString() {
     StringBuilder stringBuilder = new StringBuilder();
     for (Field field : this.getClass().getDeclaredFields()) {
       try {
         field.setAccessible(true);
-        stringBuilder.append(field.getName()).append(": ").append(field.get(this)).append("\n");
+        Object value = field.get(this);
+        if (value instanceof Printable) stringBuilder.append(ANSI_GREEN);
+        stringBuilder.append(field.getName()).append(": ");
+        stringBuilder.append(ANSI_RESET);
+        if (value instanceof Printable) stringBuilder.append("\n");
+        stringBuilder.append(value).append("\n");
       } catch (IllegalAccessException e) {
         e.printStackTrace();
       }
